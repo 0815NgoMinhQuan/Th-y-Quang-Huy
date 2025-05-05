9d550068d8f8be90f1fae78936af7fca3f598157
thaiduyvu123 <Email ca bn>
2025-05-06 00:24:25 +0700

    Cập nhật file alerts.html và các thay đổi khác

---
+++ config.py
@@ -0,0 +1,13 @@
+MYSQL_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Quan@@0815',
+    'database': 'payroll'
+}
+
+MSSQL_CONFIG = {
+    'driver': '{ODBC Driver 17 for SQL Server}',
+    'server': 'NGOMINHQUAN\\SQLEXPRESS',
+    'database': 'HUMAN_2025',
+    'trusted_cox`nnection': 'yes'
+}
